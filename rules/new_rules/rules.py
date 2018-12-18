ea2b13216c03f56ad554bba9ea9c4a43df1e237c
rom3es <roman@rhinnda.com>
2018-12-19 03:58:03 +0700

    convert XML to python 1.0

--- rules/new_rules/rules.py
+++ rules/new_rules/rules.py
@@ -1,58 +1,56 @@
 import xmltodict
+import re
 from collections import OrderedDict
 
 
 class InputData():
-    def __init__(self, xml_file):
-        with open(xml_file) as fd:
-            self._doc = xmltodict.parse(fd.read())
+    def __init__(self, data):
+        self._doc = data
 
     def get_element(self, name, root=None):
         if root is None:
             root = self._doc
 
+        if not isinstance(root, OrderedDict):
+            return InputData(None)
+
         for element_name in root:
-            if element_name == name:
-                return root[element_name]
+            raw_element_name = re.sub(r'^\w+:([\s\S]+)', r'\1', element_name)
+
+            if raw_element_name == name:
+                return InputData(root[element_name])
 
             if isinstance(root[element_name], OrderedDict):
                 result = self.get_element(name, root[element_name])
 
-                if result is not None:
+                if len(result) != 0:
                     return result
 
-        return None
+        return InputData(OrderedDict())
 
+    def __getitem__(self, index):
+        if not isinstance(self._doc, list):
+            return self._doc[index]
+        else:
+            for i in self._doc:
+                try:
+                    return i[index]
+                except KeyError:
+                    continue
 
-def rule02(data):
-    try:
-        assert data.get_element('md:NameIDFormat') in ['urn:oasis:names:tc:SAML:2.0:nameid-format:persistent', 'urn:oasis:names:tc:SAML:2.0:nameid-format:transient', '']
-    except AssertionError:
-        return 'This NameIDFormat may not be supported. Supported values for NameIDFormat are:\n    urn:oasis:names:tc:SAML:2.0:nameid-format:persistent\n    urn:oasis:names:tc:SAML:2.0:nameid-format:transient'
+            return None
 
-    return 'ok'
+    def __len__(self):
+        return len(self._doc)
 
+    def __str__(self):
+        return str(self._doc)
 
-def rule01(data):
-    def starts_with(data, pattern):
-        for p in pattern:
-            if data.startswith(p):
-                return True
-
-        return False
-
-    try:
-        assert starts_with(data.get_element('md:EntityDescriptor')['@entityID'], ['urn:', 'http://', 'https://'])
-    except AssertionError:
-        return '@entityID values should be a URI starting with http://, https:// or urn:'
-
-    return 'ok'
+    def __iter__(self):
+        return self._doc.__iter__()
 
 
 def starts_with(data, pattern, message):
-    if not isinstance(pattern, list):
-        pattern = [pattern]
-
     for p in pattern:
         if data.startswith(p):
             return 'ok'
@@ -60,12 +58,83 @@ def starts_with(data, pattern, message):
     return message
 
 
-print(rule01(InputData('../../testdata/rule01W_OK_1.xml')))
-print(rule02(InputData('../../testdata/rule02W_fail.xml')))
+def in_range(data, pattern, message):
+    if len(data) == 0:
+        return message
+
+    for d in data:
+        if str(d) not in pattern:
+            return message
+
+    return 'ok'
+
+
+# def not_in_range(data, pattern, message):
+#     for d in data:
+#         if str(d) in pattern:
+#             return message
+#
+#     return 'ok'
+
+
+def not_empty(data, message):
+    if len(data) == 0:
+        return message
+
+    for d in data:
+        if len(d) == 0:
+            return message
+
+    return 'ok'
+
+
+def get_data(path):
+    with open(path) as fd:
+        return InputData(xmltodict.parse(fd.read()))
+
+
+
+print(get_data('../../testdata/rule03W_idp_fail.xml').get_element('EntityDescriptor').get_element('ContactPerson')['@contactType'])
+
+
+exit()
 
 print(starts_with(
-    InputData('../../testdata/rule02W_fail.xml').get_element('md:EntityDescriptor')['@entityID'],
+    get_data('../../testdata/rule01W_OK_1.xml').get_element('EntityDescriptor')['@entityID'],
     ['urn:', 'http://', 'https://'],
     '@entityID values should be a URI starting with http://, https:// or urn:'
 ))
 
+
+print(in_range(
+    get_data('../../testdata/rule02W_OK.xml').get_element('NameIDFormat'),
+    [
+        'urn:oasis:names:tc:SAML:2.0:nameid-format:persistent',
+        'urn:oasis:names:tc:SAML:2.0:nameid-format:transient',
+        ''
+    ],
+    'This NameIDFormat may not be supported. Supported values for NameIDFormat are:\n    urn:oasis:names:tc:SAML:2.0:nameid-format:persistent\n    urn:oasis:names:tc:SAML:2.0:nameid-format:transient'
+))
+
+
+print(not_empty(
+    get_data('../../testdata/rule03W_idp_fail.xml').get_element('IDPSSODescriptor').get_element('NameIDFormat'),
+    'Each IDPSSODescriptor should contain NameIDFormat with one or more values'
+))
+
+
+print(not_empty(
+    get_data('../../testdata/rule03W_idp_fail.xml').get_element('SPSSODescriptor').get_element('NameIDFormat'),
+    'Each SPSSODescriptor should contain NameIDFormat with one or more values'
+))
+
+print(not_empty(
+    get_data('../../testdata/rule03W_idp_fail.xml').get_element('EntityDescriptor'),
+    'EntityDescriptor should contain an Organization'
+))
+
+print(in_range(
+    get_data('../../testdata/rule03W_idp_fail.xml').get_element('EntityDescriptor').get_element('ContactPerson')['@contactType'],
+    ['support'],
+    'EntityDescriptor should contain an Organization'
+))
